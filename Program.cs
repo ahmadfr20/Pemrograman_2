52a5a1052f35d9eb1adcab7f77df04c3073be710
ahmadfr20 <afatonir@gmail.com>
2020-11-04 10:49:35 +0700

    tugas 1 pemrograman 2

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Binatang
+{
+    class Program
+    {
+        class Binatang
+        {
+            public string name;
+            public int age;
+            public float happiness;
+
+            public void PrintBase ()
+            {
+                Console.WriteLine ("Nama:" + name);
+                Console.WriteLine ("umur:" + age);
+                Console.WriteLine ("Kebahagiaan:" + happiness);
+            }
+        }
+
+        class Kucing : Binatang
+        {
+            public int jumlahMakan;
+            public void Mengeong ()
+            {
+                Console.WriteLine("Meong");
+            }
+        }
+        class Anjing : Binatang
+        {
+            public int jumlahSpot;
+            public void Menggonggong ()
+            {
+                Console.WriteLine("WOOF"); 
+                base.happiness += 0.1f;              
+            }
+        }
+        static void Main(string[] args)
+        {
+            Anjing spike = new Anjing ();
+            spike.name = "Spike";
+            spike.age = 5;
+            spike.happiness = 0.8f;
+            spike.jumlahSpot = 20;
+            spike.PrintBase ();
+            spike.Menggonggong ();
+            Console.WriteLine ("New Happines: " + spike.happiness);
+            
+
+            Kucing tom = new Kucing ();
+            tom.name = "tom";
+            tom.age = 4;
+            tom.happiness = 0.3f;
+            tom.jumlahMakan = 20;
+            tom.happiness = 0.5f;
+            tom.PrintBase ();
+            tom.Mengeong ();
+            
+
+            Console.ReadKey ();
+        }
+    }
+}
